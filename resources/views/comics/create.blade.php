a64a919716c166c9784a36cd2fc1ad8544a8b434
luca-masera <lucamasera1995@gmail.com>
2024-01-14 22:01:59 +0100

    Correzione rotte e creazione home controller

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -7,45 +7,56 @@
 
         <section class="container">
             <h1>Crea una nuova card</h1>
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-
-
-                </div>
-
-            @endif
+            <div>
+                <a href="{{ route('comics.index') }}">Torna ai comics</a>
+            </div>
             <form action="{{ route('comics.store') }}" method="POST">
                 @csrf
 
                 <div>
                     <label for="title" class="form-label">Title</label>
-                    <input type="text" id="title" name="title" placeholder="Inserisci un title" class="form-control">
+                    <input type="text" id="title" name="title" placeholder="Inserisci un title"
+                        class="form-control @error('title') is-invalid @enderror" value="{{ old('title') }}" required>
+                    @error('title')
+                        <div class="'invalid-feedback{{ $message }} "></div>
+                    @enderror
                 </div>
                 <div>
                     <label for="description" class="form-label">Description</label>
-                    <input type="text" id="desciption" name="description" cols="20" row="5"
-                        placeholder="Inserisci una description" class="form-control">
+                    <textarea type="text" id="desciption" name="description" cols="20" row="5"
+                        placeholder="Inserisci una description" class="form-control"></textarea>
+                </div>
+                <div>
+                    <label for="thumb" class="form-label">Image:</label>
+                    <input type="url" id="thumb" name="thumb" placeholder="Inserisci una immagine"
+                        class="form-control @error('thumb') is-invalid @enderror">
+                    @error('thumb')
+                        <div class="'invalid-feedback{{ $message }} "></div>
+                    @enderror
+
                 </div>
                 <div>
                     <label for="price" class="form-label">Price:</label>
-                    <input type="price" id="price" name="price" placeholder="Inserisci un prezzo"
-                        class="form-control">
+                    <input type="text" id="price" name="price" placeholder="Inserisci un prezzo"
+                        class="form-control" required>
+                    @error('price')
+                        <div class="'invalid-feedback{{ $message }} "></div>
+                    @enderror
 
                 </div>
                 <div>
                     <label for="type" class="form-label">type:</label>
-                    <input type="text" id="type" name="type" placeholder="Inserisci un type"
-                        class="form-control">
+                    <select name="type" id="type" class="form-select" required>
+                        <option value="">Tutti</option>
+                        <option value="comic book"> comic book</option>
+                        <option value="graphic novel">graphic novel</option>
+                    </select>
 
                 </div>
 
 
                 <button type="submit">Invia</button>
+                <button type="reset">Cancella</button>
             </form>
 
         </section>
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -6,31 +6,37 @@
     <main>
 
         <section class="container">
+            <div>
+                <a href="{{ route('comics.index') }}">Torna ai comics</a>
+            </div>
             <form action="{{ route('comics.update', $comic->id) }}" method="POST">
                 @csrf
                 @method('PUT')
 
                 <div>
                     <label for="title" class="form-label">Title</label>
-                    <input type="text" id="title" name="title" placeholder="Inserisci un title" class="form-control"
-                        value="{{ old('title', $comic->title) }}">
+                    <input type="text" id="title" name="title" placeholder="Inserisci un title"
+                        class="form-control @error('title') is-invalid @enderror" value="{{ old('title', $comic->title) }}"
+                        required>
                 </div>
                 <div>
                     <label for="description" class="form-label">Description</label>
-                    <input type="text" id="desciption" name="description" cols="20" row="5"
-                        placeholder="Inserisci una description"
-                        class="form-control"value="{{ old('description', $comic->description) }}">
+                    <textarea type="text" id="desciption" name="description" cols="20" row="5"
+                        placeholder="Inserisci una description" class="form-control"value="{{ old('description', $comic->description) }}"></textarea>
                 </div>
                 <div>
                     <label for="price" class="form-label">Price:</label>
-                    <input type="price" id="price" name="price" placeholder="Inserisci un prezzo"
-                        class="form-control" value="{{ old('price', $comic->price) }}">
+                    <input type="text" id="price" name="price" placeholder="Inserisci un prezzo"
+                        class="form-control" value="{{ old('price', $comic->price) }}" required>
 
                 </div>
                 <div>
                     <label for="type" class="form-label">type:</label>
-                    <input type="text" id="type" name="type" placeholder="Inserisci un type" class="form-control"
-                        value="{{ old('type', $comic->type) }}">
+                    <select name="type" id="type" class="form-select" required>
+                        <option value="">Tutti</option>
+                        <option value="comic book"> comic book</option>
+                        <option value="graphic novel">graphic novel</option>
+                    </select>
 
                 </div>
 
