3d1757465bd83cac0cf90ed3ce1fc978eee95191
luca-masera <lucamasera1995@gmail.com>
2024-01-14 22:06:43 +0100

    Rotte web.php, HomeController & validazioni

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use \App\Models\Comic;
+
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $comics = Comic::inRandomOrder()->limit(5)->get();
+        return view('home', compact('comics'));
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComicController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +15,11 @@ use App\Http\Controllers\ComicController;
 |
 */
 
-Route::get('/', function () {
-    //$comics = config('db.comics');
-    return redirect('/comics');
-})->name('home');
+//Route::get('/', function () {
+//$comics = config('db.comics');
+//  return redirect('/comics');
+//})->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::resource('comics', ComicController::class);
 
